8b94f83f32f44cc4953f63e2a68ae542129223ec
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-03-06 10:06:50 -0500

    School Commit Mar6

---
+++ src/assignment4/A4Q1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author laveh2107
+ */
+public class A4Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Scanner to get input
+        Scanner input = new Scanner(System.in);
+        
+        //Ask user to type in name
+        System.out.println("Please enter the name you were given at birth");
+        
+        //set name givin as variable
+        String name = input.nextLine();
+    }
+}
