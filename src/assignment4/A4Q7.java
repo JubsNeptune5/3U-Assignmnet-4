42e9899e73f76f2008a518e3e01d38cc925cff9e
hunterlaverty <hunterlaverty@192.168.0.20>
2017-04-06 18:01:14 -0400

    Commit home April 6

--- src/assignment4/A4Q7.java
+++ src/assignment4/A4Q7.java
@@ -23,6 +23,8 @@ public class A4Q7 {
         // initaluiza a start position
         int p = 1;
 
+        // Add a counter 
+        int x = 0;
         //Ask to enter dice
         System.out.println("Welcome to Snakes and Latters!!!!");
         while (p < 100) {
@@ -39,26 +41,33 @@ public class A4Q7 {
 
                     //if marker lands on a ladder
                     if (p == 9) {
+                        System.out.println("Yay, You climbed a latter");
                         p = 34;
                     }
                     if (p == 40) {
+                        System.out.println("Yay, You climbed a latter");
                         p = 64;
                     }
                     if (p == 67) {
+                        System.out.println("Yay, You climbed a latter");
                         p = 86;
                     }
 
                     //if land on snakes 
                     if (p == 99) {
+                        System.out.println("OH No you hit a snake");
                         p = 77;
                     }
                     if (p == 90) {
+                        System.out.println("OH No you hit a snake");
                         p = 48;
                     }
                     if (p == 54) {
+                        System.out.println("OH No you hit a snake");
                         p = 19;
                     }
                 }
+            x++;
             }
             //Quit the game
             if (dice == 0) {
@@ -72,6 +81,7 @@ public class A4Q7 {
             //Iff score is above or exactly 100, say they won
             if (p >= 100) {
                 System.out.println("Congratulations, you won the game!!!");
+                System.out.println("And it only took you " +x+ " rolls");
             }
         }
 
