9d7f636210bc5e57baefa34c90007a4b65b4f2ae
hunterlaverty <hunterlaverty@192.168.0.20>
2017-04-06 15:17:33 -0400

    Revision, done at home April 6

--- src/assignment4/A4Q7.java
+++ src/assignment4/A4Q7.java
@@ -17,8 +17,7 @@ public class A4Q7 {
      */
     public static void main(String[] args) {
 
-            
-            for(int x=1; x<100; x=x+1){
+        for (int x = 1; x < 100; x = x + 1) {
 
             // Scanner to get input
             Scanner input = new Scanner(System.in);
@@ -29,41 +28,40 @@ public class A4Q7 {
 
             if (dice == 0) {
                 System.out.println("You choose to quit the game");
-             break;
-            } 
+                break;
+            }
             //Make sure the dice value is between 2 and 12
             if (dice >= 2) {
                 if (dice <= 12) {
-                    
-                int spot = 1 + dice;
-                
-                //if marker lands on a ladder
-                if (spot == 9){
-                    spot = 34;
-                }
-                if(spot ==40){
-                    spot = 64;
-                }
-                if(spot == 67){
-                    spot = 86;
-                }
-                
-                //if land on snakes 
-                if (spot == 99){
-                    spot = 77;
-                }
-                if ( spot == 90){
-                    spot = 48;
-                }
-                if (spot == 54){
-                    spot = 19;
-                }
-                if (spot < 100){
-                    System.out.println("You are now on square " +spot);
-                }
+
+                    int spot = 1 + dice;
+
+                    //if marker lands on a ladder
+                    if (spot == 9) {
+                        spot = 34;
+                    }
+                    if (spot == 40) {
+                        spot = 64;
+                    }
+                    if (spot == 67) {
+                        spot = 86;
+                    }
+
+                    //if land on snakes 
+                    if (spot == 99) {
+                        spot = 77;
+                    }
+                    if (spot == 90) {
+                        spot = 48;
+                    }
+                    if (spot == 54) {
+                        spot = 19;
+                    }
+                    if (spot < 100) {
+                        System.out.println("You are now on square " + spot);
+                    }
                 }
             }
-            } 
         }
     }
-
+}
